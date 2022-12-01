d42bebc3a42052d61da75464eab21d346fc40396
Maksim Dzjubenko <90320393+SvinDzjuba@users.noreply.github.com>
2022-12-01 21:10:45 +0200

    Finished the insert data method

--- data/insert_data.js
+++ data/insert_data.js
@@ -4,6 +4,8 @@ const Employee = require('../models/employee');
 const Position = require('../models/position');
 const Product = require('../models/product');
 const Shop = require('../models/shop');
+const ShopEmployee = require('../models/shop_employee');
+const ShopProduct = require('../models/shop_product');
 
 module.exports.insertData = async () => {
     const jsonStr = JSON.stringify(jsonData);
@@ -32,12 +34,29 @@ module.exports.insertData = async () => {
     }
 
     for (let i = 0; i < data.length; i++) {
-        // Find all positions
+        // Find shop for its id
+        let shop = await Shop.findOne({
+            where: {
+                name: data[i].name,
+                address: data[i].address
+            },
+            attributes: ['id']
+        });
+        // Create shop if it doesn't exist
+        if(shop == null) {
+            shop = await Shop.create({
+                name: data[i].name,
+                address: data[i].address
+            });
+        }
+
         for (let j = 0; j < employees[i].length; j++) {
+            // Find particular position id for product.position
             let position = await Position.findOne({
                 where: { name: employees[i][j].position },
                 attributes: ['id']
             });
+            // Create employee if it doesn't exist
             await Employee.findOrCreate({
                 where: {
                     fullName: employees[i][j].fullName,
@@ -45,20 +64,54 @@ module.exports.insertData = async () => {
                     position: position.id
                 }
             });
+            // Find employee id to create relation between shop and employee
+            let employee = await Employee.findOne({
+                where: {
+                    fullName: employees[i][j].fullName,
+                    age: employees[i][j].age,
+                    position: position.id
+                },
+                attributes: ['id']
+            })
+            // Create relations between shop and employees
+            await ShopEmployee.findOrCreate({
+                where: {
+                    shopId: shop.id,
+                    employeeId: employee.id
+                }
+            });
         }
 
-        // Find all classifications
         for (let j = 0; j < products[i].length; j++) {
+            // Find particular classification id for product.classification
             let classification = await Classification.findOne({
                 where: { name: products[i][j].classification },
                 attributes: ['id']
             });
+            // Create product if it doesn't exist
             await Product.findOrCreate({
                 where: {
                     name: products[i][j].name,
                     classification: classification.id,
                     price: products[i][j].price,
                     description: products[i][j].description,
+                }
+            });
+            // Find product id to create relation between shop and product
+            let product = await Product.findOne({
+                where: {
+                    name: products[i][j].name,
+                    classification: classification.id,
+                    price: products[i][j].price,
+                    description: products[i][j].description,
+                },
+                attributes: ['id']
+            })
+            // Create relations between shop and products
+            ShopProduct.findOrCreate({
+                where: {
+                    productId: product.id,
+                    shopId: shop.id
                 }
             });
         }
